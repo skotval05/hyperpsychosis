129feb68d442ffd1db7320c81d3eefcaae8bc551
The Grungler <jingerjesus420@gmail.com>
2025-03-02 20:12:24 -0500

    got the rainbow triangle!

--- graphics/graphics.c
+++ graphics/graphics.c
@@ -2,6 +2,7 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <math.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow * window);
@@ -12,15 +13,19 @@ const unsigned int SCREENHEIGHT = 600;
 const char* vertexShaderSource = 
         "#version 330 core \n"
         "layout (location =0) in vec3 aPos;\n"
+        "layout (location =1) in vec3 aColor;\n"
+        "out vec3 ourColor;\n"
         "void main() {\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "   gl_Position = vec4(aPos, 1.0);\n"
+        "   ourColor=aColor;\n"
         "}\0";
 
 const char* fragmentShaderSource = 
         "#version 330 core\n"
         "out vec4 FragColor;\n"
+        "in vec3 ourColor;\n"
         "void main() {\n"
-        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "   FragColor = vec4(ourColor, 1.0);\n"
         "}\0";
 
 int doGraphicsSetup() {
@@ -58,21 +63,51 @@ int doGraphicsSetup() {
         return -1;
     }
     
-    // test triangle
+    // two triangle,,
     float vertices[] = {
-    -0.5f, -0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-     0.0f,  0.5f, 0.0f
+         // positions         // colors
+         0.5f, -0.5f,  0.0f,  1.0f, 0.0f, 0.0f, // bottom right
+        -0.5f, -0.5f,  0.0f,  0.0f, 1.0f, 0.0f, // bottom left
+         0.0f,  0.5f,  0.0f,  0.0f, 0.0f, 1.0f  // top
     };
 
-    // generate vertex buffer object (stores vertices)
-    unsigned int VBO;
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    unsigned int indices [] = {
+    //0, 1, 3,
+    0, 1, 2
+    };
 
-    // maybe something goes here, but now its telling me to buffer data.
+    // let's do all the buffers and vertex shenanigans in one shot.
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // bind vertex array object first, then bind set vertex buffs, then configure attributes.
     
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute of each vertex
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // color attribute of each vertex
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // this is allowed? considered a safe unbind, for whatever reason.
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // DO NOT UNBIND EBO WHILE VAO IS ACTIVE AS THE BOUND ELEMENT?
+    // dont unbind vaos or vbos when it is not directly necessary.
+    glBindVertexArray(0);
+
 
     // vertex shader. is programmable. uses GLSL, no idea if that'll be foreshadowing of a painful future.
 
@@ -147,25 +182,8 @@ int doGraphicsSetup() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    // now comes linking vertex attributes. oh no.
-    
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
-    // make da vertex array object :3
-
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-
-    // initialization code done once unless the object frequently changes...
-
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+    // uncomment to see wireframe
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // DA RENDER LOOP
 
@@ -178,10 +196,18 @@ int doGraphicsSetup() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // draw triangle number 1
+        // be sure to activate shader
         glUseProgram(shaderProgram);
+
+        // update uniform color
+        float timeValue = glfwGetTime();
+        float greenValue = sin(timeValue) / 2.0f + 0.5f;
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
+        // NOW render triangle.
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         // swap buffers and poll IO events
         glfwSwapBuffers(window);
